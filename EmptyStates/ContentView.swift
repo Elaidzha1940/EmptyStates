5227265f520edc1e3f2784cab4c3a2634e22358c
Elaidzha1940 <eli080293@gmail.com>
2023-12-29 17:28:35 +0300

    EmptyStates

---
+++ EmptyStates/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: EmptyStates
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.12.2023
+//
+//  */ 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmptyStates/EmptyStatesApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: EmptyStates
+//  File: EmptyStatesApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct EmptyStatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
